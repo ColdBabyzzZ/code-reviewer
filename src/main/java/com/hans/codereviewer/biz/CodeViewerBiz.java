d1d572625565e5b4e920ba54c3b94018d9e2cd1d
ColdBaby <huihan880@gmail.com>
2024-10-29 23:03:37 +0800

    test webhook

--- src/main/java/com/hans/codereviewer/biz/CodeViewerBiz.java
+++ src/main/java/com/hans/codereviewer/biz/CodeViewerBiz.java
@@ -1,9 +1,13 @@
 package com.hans.codereviewer.biz;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CodeViewerBiz {
+
+    @Autowired
+    private DeepSeekBIz deepSeekBIz;
 
     public void codeReview(String gitContent) {
 
